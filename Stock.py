c06ee0882cfd35ae4da4f8daf77cdcc4ddc87672
Mahendra Ramachandran <praboo@gmail.com>
2016-05-12 17:43:02 -0700

    Updated Stock to CVX
    And adding additional analysis

--- Stock.py
+++ Stock.py
@@ -19,7 +19,7 @@ end = datetime.datetime(2015, 8, 4)
 
 # Get data
 #data = pandas.io.data.DataReader('CVX', 'yahoo', start, end)
-data = pandas.io.data.DataReader('OIL', 'yahoo', start)
+data = pandas.io.data.DataReader('CVX', 'yahoo', start)
 # Get just the close
 close = data['Adj Close']
 
@@ -36,35 +36,35 @@ close.plot()
 # Adding Additional Data
 
 # Get the difference in price from previous step
-#delta = close.diff()
-## Get rid of the first row, which is NaN since it did not have a previous 
-## row to calculate the differences
-#delta = delta[1:] 
-#
-## Make the positive gains (up) and negative gains (down) Series
-#up, down = delta.copy(), delta.copy()
-#up[up < 0] = 0
-#down[down > 0] = 0
-#
-## Calculate the EWMA
-#roll_up1 = pandas.stats.moments.ewma(up, window_length)
-#roll_down1 = pandas.stats.moments.ewma(down.abs(), window_length)
-#
-## Calculate the RSI based on EWMA
-#RS1 = roll_up1 / roll_down1
-#RSI1 = 100.0 - (100.0 / (1.0 + RS1))
-#
-## Calculate the SMA
-#roll_up2 = pandas.rolling_mean(up, window_length)
-#roll_down2 = pandas.rolling_mean(down.abs(), window_length)
-#
-## Calculate the RSI based on SMA
-#RS2 = roll_up2 / roll_down2
-#RSI2 = 100.0 - (100.0 / (1.0 + RS2))
-#
-## Compare graphically
-#plt.figure()
-#RSI1.plot()
-#RSI2.plot()
-#plt.legend(['RSI via EWMA', 'RSI via SMA'])
-#plt.show()
+delta = close.diff()
+# Get rid of the first row, which is NaN since it did not have a previous 
+# row to calculate the differences
+delta = delta[1:] 
+
+# Make the positive gains (up) and negative gains (down) Series
+up, down = delta.copy(), delta.copy()
+up[up < 0] = 0
+down[down > 0] = 0
+
+# Calculate the EWMA
+roll_up1 = pandas.stats.moments.ewma(up, window_length)
+roll_down1 = pandas.stats.moments.ewma(down.abs(), window_length)
+
+# Calculate the RSI based on EWMA
+RS1 = roll_up1 / roll_down1
+RSI1 = 100.0 - (100.0 / (1.0 + RS1))
+
+# Calculate the SMA
+roll_up2 = pandas.rolling_mean(up, window_length)
+roll_down2 = pandas.rolling_mean(down.abs(), window_length)
+
+# Calculate the RSI based on SMA
+RS2 = roll_up2 / roll_down2
+RSI2 = 100.0 - (100.0 / (1.0 + RS2))
+
+# Compare graphically
+plt.figure()
+RSI1.plot()
+RSI2.plot()
+plt.legend(['RSI via EWMA', 'RSI via SMA'])
+plt.show()
